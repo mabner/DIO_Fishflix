486cb5b3de0b6df02b151300e0a667b19822f52c
Marcos Leite <m.abner@live.co.uk>
2022-02-14 01:38:16 -0300

    renames cover images and fixes the image carousel

--- js/tiny-slider-setup.js
+++ js/tiny-slider-setup.js
@@ -1,10 +1,26 @@
 var slider = tns({
 	container: ".my-slider",
-	items: 3,
+	items: 4,
+	edgePadding: 10,
+	gutter: 10,
 	slideBy: "page",
 	mouseDrag: true,
 	swipeAngle: false,
 	autoplay: true,
 	autoplayButtonOutput: false,
 	controls: false,
+	nav: false,
+	speed: 400,
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 3
+		},
+		1024: {
+			items: 5
+		}
+	},
+	loop: true,
 });
